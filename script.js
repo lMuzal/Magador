0ab258cb7ccdaa92b6380df6f542dbe5a5aa2af1
lMuzal <muza.jakub@gmail.com>
2022-02-13 12:59:04 +0100

    new changes

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const temp = document.getElementById('temp');
 const tempMax = document.getElementById('temp-max');
 const tempFeel = document.getElementById('temp-feel');
 const humidity = document.getElementById('humidity');
+const wind = document.getElementById('wind');
+const windDir = document.getElementById('wind-dir');
 const pressure = document.getElementById('pressure');
 const weatherBtn = document.getElementById('weather-button');
 const weatherCloseBtn = document.getElementById('overlay-close-button');
@@ -15,25 +17,82 @@ const reservationsBtn = document.getElementById('reservations');
 const contactBtn = document.getElementById('contact');
 const logo = document.querySelector('.loader');
 
+
+
 (function getWeather(){
     console.log("click");
     fetch(`https://api.openweathermap.org/data/2.5/weather?id=3081752&units=metric&appid=${weatherApiKey}&lang=pl`)
-        .then(function(res){
-            return res.json();
-        })
-        .then(function(data){
-            console.log(data);
-            description.innerHTML = data.weather[0].description;
-            const weatherIcons = data.weather[0].icon;
-            weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weatherIcons}@2x.png">`;
-            tempMin.innerHTML = `Min.<br>${data.main.temp_min}<span style= "font-family: 'Segoe UI Symbol'">&#8451<span>`;
-            temp.innerHTML = `<span style="font-size: 20px">Temp.</span><br>${data.main.temp}<span style= "font-family: 'Segoe UI Symbol'">&#8451<span>`;
-            tempMax.innerHTML = `Max.<br>${data.main.temp_max}<span style= "font-family: 'Segoe UI Symbol'">&#8451<span>`;
-            tempFeel.innerHTML = `Temp. odczuwalna: ${data.main.feels_like}<span style= "font-family: 'Segoe UI Symbol'">&#8451<span>`;
-            humidity.innerHTML = `Wilgotność: ${data.main.humidity} %`;
-            pressure.innerHTML = `Ciśnienie: ${data.main.pressure} hPa`;
+        .then(res => res.json())
+        .then(data => {
+                console.log(data);
+                description.innerHTML = data.weather[0].description;
+                const weatherIcons = data.weather[0].icon;
+                weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weatherIcons}@2x.png">`;
+                tempMin.innerHTML = `Min.<br>${data.main.temp_min}<span style= "font-family: 'Segoe UI Symbol'">&#8451<span>`;
+                temp.innerHTML = `<span style="font-size: 20px">Temp.</span><br>${data.main.temp}<span style= "font-family: 'Segoe UI Symbol'">&#8451<span>`;
+                tempMax.innerHTML = `Max.<br>${data.main.temp_max}<span style= "font-family: 'Segoe UI Symbol'">&#8451<span>`;
+                tempFeel.innerHTML = `Temp. odczuwalna: ${data.main.feels_like}<span style= "font-family: 'Segoe UI Symbol'">&#8451<span>`;
+                humidity.innerHTML = `Wilgotność: ${data.main.humidity} %`;
+                pressure.innerHTML = `Ciśnienie: ${data.main.pressure} hPa`;
+                wind.innerHTML = `Wiatr (w por.): ${data.wind.speed}(${data.wind.gust}) m/s`;
+                
+                (() => {
+                    const windDeg = data.wind.deg;
+                    console.log(windDeg);
 
-        })
+                    if (windDeg >= 348 && windDeg <= 360 || windDeg >= 0 && windDeg <= 11) {
+                        windDir.innerHTML = '<p>Kierunek wiatru</p><i class="fas fa-chevron-circle-down"></i> N</i>';
+                    }
+                    else if (windDeg >= 11 && windDeg <= 33) {
+                        windDir.innerHTML = '<p>Kierunek wiatru</p><i class="fas fa-chevron-circle-down" style= "transform: rotate(22.5deg)"></i> NNE</i>';
+                    }
+                    else if (windDeg >= 33 && windDeg <= 56) {
+                        windDir.innerHTML = '<p>Kierunek wiatru</p><i class="fas fa-chevron-circle-down" style= "transform: rotate(45deg)"></i> NE</i>';
+                    }
+                    else if (windDeg >= 56 && windDeg <= 78) {
+                        windDir.innerHTML = '<p>Kierunek wiatru</p><i class="fas fa-chevron-circle-down" style= "transform: rotate(67.5deg)"></i> ENE</i>';
+                    }
+                    else if (windDeg >= 78 && windDeg <= 101) {
+                        windDir.innerHTML = '<p>Kierunek wiatru</p><i class="fas fa-chevron-circle-down" style= "transform: rotate(90deg)"></i> E</i>';
+                    }
+                    else if (windDeg >= 101 && windDeg <= 123) {
+                        windDir.innerHTML = '<p>Kierunek wiatru</p><i class="fas fa-chevron-circle-down" style= "transform: rotate(112.5deg)"></i> ESE</i>';
+                    }
+                    else if (windDeg >= 123 && windDeg <= 146) {
+                        windDir.innerHTML = '<p>Kierunek wiatru</p><i class="fas fa-chevron-circle-down" style= "transform: rotate(135deg)"></i> SE</i>';
+                    }
+                    else if (windDeg >= 146 && windDeg <= 168) {
+                        windDir.innerHTML = '<p>Kierunek wiatru</p><i class="fas fa-chevron-circle-down" style= "transform: rotate(157.5deg)"></i> SSE</i>';
+                    }
+                    else if (windDeg >= 168 && windDeg <= 191) {
+                        windDir.innerHTML = '<p>Kierunek wiatru</p><i class="fas fa-chevron-circle-down" style= "transform: rotate(180deg)"></i> S</i>';
+                    }
+                    else if (windDeg >= 191 && windDeg <= 213) {
+                        windDir.innerHTML = '<p>Kierunek wiatru</p><i class="fas fa-chevron-circle-down" style= "transform: rotate(202.5deg)"></i> SSW</i>';
+                    }
+                    else if (windDeg >= 213 && windDeg <= 236) {
+                        windDir.innerHTML = '<p>Kierunek wiatru</p><i class="fas fa-chevron-circle-down" style= "transform: rotate(225deg)"></i> SW</i>';
+                    }
+                    else if (windDeg >= 236 && windDeg <= 258) {
+                        windDir.innerHTML = '<p>Kierunek wiatru</p><i class="fas fa-chevron-circle-down" style= "transform: rotate(247.5deg)"></i> WSW</i>';
+                    }
+                    else if (windDeg >= 258 && windDeg <= 281) {
+                        windDir.innerHTML = '<p>Kierunek wiatru</p><i class="fas fa-chevron-circle-down" style= "transform: rotate(270deg)"></i> W</i>';
+                    }
+                    else if (windDeg >= 281 && windDeg <= 303) {
+                        windDir.innerHTML = '<p>Kierunek wiatru</p><i class="fas fa-chevron-circle-down" style= "transform: rotate(292.5deg)"></i> WNW</i>';
+                    }
+                    else if (windDeg >= 303 && windDeg <= 326) {
+                        windDir.innerHTML = '<p>Kierunek wiatru</p><i class="fas fa-chevron-circle-down" style= "transform: rotate(315deg)"></i> NW</i>';
+                    }
+                    else if (windDeg >= 326 && windDeg <= 348) {
+                        windDir.innerHTML = '<p>Kierunek wiatru</p><i class="fas fa-chevron-circle-down" style= "transform: rotate(337.5deg)"></i> NNW</i>';
+                    }
+                    else {
+                        windDir.innerHTML = '<i class="fas fa-wind"></i>';
+                    }
+                })()
+            })    
 })();
 
 
@@ -48,9 +107,9 @@ function windguruForecast(){
 weatherCloseBtn.addEventListener("click", closeOverlay);
 
 function closeOverlay(){
-
+    const link = window.location.href
     document.querySelector('#overlay').style.display = "none";
-    window.location.href = history.back();
+    window.location.href = link;
 }
 
 logo.addEventListener("click", logoClick);
